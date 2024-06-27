893e493963c73d39d6011463e5f4d904ddde80d3
jeancabouat <jean.cabouat@gmail.com>
2024-06-27 16:13:02 +0000

    Test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 st.set_page_config(layout="wide")
 
-# Load the HTML file
+# Load the HTML file (map)
 def read_html_file(filename):
     with open(filename, 'r') as f:
         return f.read()
